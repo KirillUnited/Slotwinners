2d0e5f119b7bda73483fe4c61910d9606d0697e0
k.dubov <example@example.com>
2022-09-10 08:47:07 +0300

    Update form, header

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -4,14 +4,14 @@ const Form = ({id}) => {
     return (
         <form className="form" id={id} method="POST" action="">
             <fieldset className="form-fieldset">
-                <FormInput type="text" id={`first_name`} placeholder="First name" required/>
-                <FormInput type="text" id={`last_name`} placeholder="Last name"/>
-                <FormInput type="email" id={`email`} placeholder="Email" required/>
-                <FormInput type="phone" id={`phone`} placeholder="Phone number"/>
-                <FormInput type="text" id={`address`} placeholder="Address"/>
-                <FormInput type="text" id={`birthdate`} placeholder="Birthdate"/>
-                <FormInput type="password" id={`password`} placeholder="Password" required/>
-                <FormInput type="password" id={`confirm_password`} placeholder="Confirm password" required/>
+                <FormInput type="text" id={`first_name`} name={`first_name`} placeholder="First name" required/>
+                <FormInput type="text" id={`last_name`} name={`last_name`} placeholder="Last name"/>
+                <FormInput type="email" id={`email`} name={`email`} placeholder="Email" required/>
+                <FormInput type="phone" id={`phone`} name={`phone`} placeholder="Phone number"/>
+                <FormInput type="text" id={`address`} name={`address`} placeholder="Address"/>
+                <FormInput type="text" id={`birthdate`} name={`birthdate`} placeholder="Birthdate"/>
+                <FormInput type="password" id={`password`} name={`password`} placeholder="Password" required/>
+                <FormInput type="password" id={`confirm_password`} name={`confirm_password`} placeholder="Confirm password" required/>
             </fieldset>
         </form>
     )
