d0d39cafcd3df7110a1f2712eebcc4f118e5b5a8
k.dubov <example@example.com>
2022-09-13 23:48:48 +0300

    Update privacy

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import "@fontsource/inter/700.css";
 import "@fontsource/noto-sans/400.css";
 import "@fontsource/noto-sans/600.css";
+import "@fontsource/noto-sans/700.css";
 import "@fontsource/syne/500.css";
 import "@fontsource/syne/600.css";
 import "@fontsource/syne/700.css";
